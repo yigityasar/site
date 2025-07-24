f56102e4c0b6fc9730ef99c8126b54fe38185015
Yiğit Yaşar <yigityasar.dev@gmail.com>
2025-07-24 17:29:29 +0300

    site
    site

---
+++ data.js
@@ -0,0 +1,28 @@
+window.siteData = {
+  male: { 
+    name: "Yiğit",
+    photo: "img/erkek.jpeg" // Erkek fotoğrafı
+  },
+  female: { 
+    name: "Azra",
+    photo: "img/kadin.jpeg" // Kadın fotoğrafı
+  },
+  anniversaryDate: "2024-10-28",
+  loveMessages: [
+    "Seni çok seviyorum!",
+    "Birlikte nice güzel günlere.",
+    "Her gün seninle daha güzel."
+  ],
+  couplePhotos: [
+    "img/foto (1).jpeg",
+    "img/foto (2).jpeg",
+    "img/foto (3).jpeg",
+    "img/foto (4).jpeg",
+    "img/foto (5).jpeg",
+    "img/foto (6).jpeg",
+    "img/foto (7).jpeg",
+    "img/foto (8).jpeg",
+    "img/foto (9).jpeg",
+    "img/foto (10).jpeg"
+  ]
+};
