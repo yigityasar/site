a9a82a28e1c44aef7776616b1c52500522db25bf
Yiğit Yaşar <yigityasar.dev@gmail.com>
2025-07-24 17:48:18 +0300

    site
    v2

--- data.js
+++ data.js
@@ -10,7 +10,7 @@ window.siteData = {
   anniversaryDate: "2024-10-28",
   loveMessages: [
     "Seni çok seviyorum!",
-    "Birlikte nice güzel günlere.",
+    "Seninle tanıştığım gün, hayatımın renk paletine yeni bir ton eklendi: Azra tonu. Ne geçmişin eksik kaldı, ne de gelecekten korktum sen yanımdayken. Uzaklar bazen yoruyor ama her 00:00’da attığın ‘seni seviyorum’ mesajı, bütün yolları kısaltıyor. Kalbim sana kurye gibi çalışıyor: sadece tek adrese teslim, hep sana.",
     "Her gün seninle daha güzel."
   ],
   couplePhotos: [
